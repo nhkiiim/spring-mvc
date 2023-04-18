673143e7e749bab9fdc072ebb83f724ab9021a75
nhkiiim <nhkiiim@naver.com>
2023-04-18 22:23:40 +0900

    Add: 단순 텍스트 API 방식 조회 서블릿

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // HTTP 메시지 바디의 내용을 바이트코드로 얻을 수 있음
+        ServletInputStream inputStream = request.getInputStream();
+
+        // 바이트코드 -> String (인코딩 정보 필요)
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write(messageBody);
+
+    }
+}
