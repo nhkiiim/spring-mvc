7aa29107262ee16eecbffad575bdf075cd5e5c6c
nhkiiim <nhkiiim@naver.com>
2023-05-07 19:44:07 +0900

    Refactor: FrontController로 View 분리

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -7,8 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface ControllerV2 {
+public interface  ControllerV2 {
 
+    // MyView 반환
     MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 }
